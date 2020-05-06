c44a1fb25edede1c24e664aac92c6cb64b72e73c
William <wei.william@uwaterloo>
2020-05-05 20:04:39 -0400

    Changed layout.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,24 @@ import '../Styles/Header.css'
 const Header = ({year, month}) =>
     <div className="header"> 
         <h1>Gacha Rank</h1>
-        <p>Ranking of gacha games.</p>
         <p>Current Stats: {month} {year}</p>
+        <div className="menu">
+            <div className="menuBox">
+                <p className="hRank">#</p>
+                <p className="hTitle">Name</p>
+                <p className="hPublisher">Publisher</p>
+                <div className="downloads">
+                    <p>Monthly Downloads</p>
+                    <div className="downloadsBox">
+                        <p className="hAppleDownloads">IOS</p>
+                        <p>Android</p>
+                    </div>
+                </div>
+                <p className="hRevenue">Revenue</p>
+                <p>Links</p>
+            </div>
+        </div>
+        <hr />
     </div>
     
 export default Header
--- src/Components/Listing.js
+++ src/Components/Listing.js
@@ -3,20 +3,20 @@ import '../Styles/Listing.css'
 
 const Listing = ({obj, rank}) => 
     <div className="content">
-        <div className="left">
-            <img className="icon" src={obj.img} alt="App icon."/>
-            <p className="rank">Rank: {rank}</p>
-        </div>
-        <div className="info">    
-            <p className="title">{obj.title}</p>
-            <p>{obj.publisher}</p>     
-            <p>IOS Downloads: {obj.apple}</p>
-            <p>Android Downloads: {obj.google}</p>
-            <p>Revenue: {obj.revenue}</p>
-            <a href={obj.iosLink} target="_blank" rel="noopener noreferrer">iTunes</a> 
-            &nbsp;
+        <p className="rank">{rank}</p> 
+        <img className="icon" src={obj.img} alt="App icon."/>   
+        <p className="title">{obj.title}</p>
+        <p className="publisher">{obj.publisher}</p>     
+        <p className="appleDownloads">{obj.apple}</p>
+        <p className="googleDownloads">{obj.google}</p>
+        <p className="revenue">{obj.revenue}</p>
+        <p>
+            <a href={obj.iosLink} target="_blank" rel="noopener noreferrer">iTunes</a>
+        </p>
+        &nbsp;
+        <p>
             <a href={obj.androidLink} target="_blank" rel="noopener noreferrer">Google Play</a>
-        </div>
+        </p>
     </div>
 
 export default Listing
