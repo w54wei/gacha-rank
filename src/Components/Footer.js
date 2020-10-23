79ee5866832a38233c8059d1ee0a4d9ed5ce23a3
William <wei.william@uwaterloo>
2020-10-23 03:31:47 -0400

    Improved design.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import '../Styles/Footer.css'
 
 const Footer = () =>
-    <div> 
+    <div className="footer"> 
         <p>© Gacha Rank, 2020. Data acquired from <a href='https://sensortower.com/' target='_blank'>sensortower.com</a>. 
         Please email: contact@gacharank.com for inquiries.</p>
         <p>Legal Disclaimer: The data presented here are estimates and may not accurately reflect company statistics.</p>
