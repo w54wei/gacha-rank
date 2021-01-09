8f5ab20a75038d44b3c1590d86a9d991b53d1daf
William <wei.william@uwaterloo>
2021-01-09 04:29:13 -0800

    Change analytics.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-//import ReactGA from 'react-ga'
-
-//ReactGA.initialize('UA-162538032-3')
-//ReactGA.pageview(window.location.pathname + window.location.search)
 
 ReactDOM.render(
   <React.StrictMode>
